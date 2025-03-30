6c9454498b68a4116c69b6b416255a20a823c73e
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-03-30 17:33:44 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: "2025-03-29",
   devtools: { enabled: true },
   css: ["/assets/css/main.css"],
   vite: {
