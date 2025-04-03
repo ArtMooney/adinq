11d9441dd8d6706f7323878554306e66c6177894
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-04-03 22:38:55 +0200

    work on site styling

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="relative ml-[calc(-50vw+50%)] h-full w-screen grow">
+  <div class="relative mx-[calc(-50vw+50%)] h-full w-screen grow">
     <ClientOnly>
       <video
         autoplay
@@ -31,6 +31,15 @@ definePageMeta({
         <source src="../assets/videos/folkmassa.mp4" type="video/mp4" />
       </video>
       <div class="absolute inset-0 bg-black/40"></div>
+
+      <div
+        class="absolute inset-0 flex flex-col items-center justify-center bg-gray-800/50"
+      >
+        <h1 class="mb-8 text-7xl">Vi hjälper er att nå ut digitalt!</h1>
+        <div class="text-2xl">
+          Lokalt, regionalt eller rikstäckande - just DOOH* it!
+        </div>
+      </div>
     </ClientOnly>
   </div>
 </template>
