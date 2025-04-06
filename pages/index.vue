e36c2fc14a071717ac218ea5a519567875898324
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-04-06 19:05:50 +0200

    creating structure and design for index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <script setup>
-import { TvIcon } from "@heroicons/vue/24/outline";
+import {
+  TvIcon,
+  ComputerDesktopIcon,
+  VideoCameraIcon,
+  BuildingOffice2Icon,
+} from "@heroicons/vue/24/outline";
 
 useSeoMeta({
   title: "",
@@ -47,13 +52,65 @@ definePageMeta({
       </div>
     </div>
 
-    <div class="mt-12 mb-20 grid grid-cols-2 gap-4">
+    <div class="mt-12 mb-20 grid grid-cols-2 gap-8">
       <div
-        v-for="i in 4"
-        :key="i"
-        class="min-h-64 rounded border border-white/25 bg-neutral-900 p-4"
+        class="group relative min-h-52 rounded border border-white/25 bg-neutral-900 p-8 pb-12"
       >
-        <TvIcon class="h-10 w-10" />
+        <div
+          class="absolute inset-0 z-[-1] rounded-xl bg-gradient-to-r from-pink-500 to-purple-500 opacity-0 blur-xl transition-opacity duration-300 ease-in-out group-hover:opacity-50"
+        ></div>
+        <TvIcon class="relative mb-3 h-10 w-10" />
+        <div class="relative mb-6 h-[1px] w-2/3 bg-white/25"></div>
+        <p class="relative">
+          Butiks-TV är ett effektivt marknadsföringsverktyg som visar reklam och
+          information direkt i butiksmiljön. Skärmarna placeras strategiskt för
+          att nå kunder när de handlar.
+        </p>
+      </div>
+
+      <div
+        class="group relative min-h-52 rounded border border-white/25 bg-neutral-900 p-8 pb-12"
+      >
+        <div
+          class="absolute inset-0 z-[-1] rounded-xl bg-gradient-to-r from-pink-500 to-purple-500 opacity-0 blur-xl transition-opacity duration-300 ease-in-out group-hover:opacity-50"
+        ></div>
+        <ComputerDesktopIcon class="relative mb-3 h-10 w-10" />
+        <div class="relative mb-6 h-[1px] w-2/3 bg-white/25"></div>
+        <p class="relative">
+          Digitala utomhusskärmar eller LED-skyltar visar rörlig reklam som
+          effektivt fångar förbipasserande trafikanters uppmärksamhet. Med sina
+          starka färger och dynamiska innehåll skapar de minnesvärda intryck
+          även vid hög hastighet.
+        </p>
+      </div>
+
+      <div
+        class="group relative min-h-52 rounded border border-white/25 bg-neutral-900 p-8 pb-12"
+      >
+        <div
+          class="absolute inset-0 z-[-1] rounded-xl bg-gradient-to-r from-pink-500 to-purple-500 opacity-0 blur-xl transition-opacity duration-300 ease-in-out group-hover:opacity-50"
+        ></div>
+        <VideoCameraIcon class="relative mb-3 h-10 w-10" />
+        <div class="relative mb-6 h-[1px] w-2/3 bg-white/25"></div>
+        <p class="relative">
+          Med specialistkompetens kring olika kanaler maximeras avkastningen på
+          kundens marknadsföringsinvesteringar genom datadrivna beslut och
+          förhandlingskraft gentemot medieleverantörer.
+        </p>
+      </div>
+
+      <div
+        class="group relative min-h-52 rounded border border-white/25 bg-neutral-900 p-8 pb-12"
+      >
+        <div
+          class="absolute inset-0 z-[-1] rounded-xl bg-gradient-to-r from-pink-500 to-purple-500 opacity-0 blur-xl transition-opacity duration-300 ease-in-out group-hover:opacity-50"
+        ></div>
+        <BuildingOffice2Icon class="relative mb-3 h-10 w-10" />
+        <div class="relative mb-6 h-[1px] w-2/3 bg-white/25"></div>
+        <p class="relative">
+          Strategiska samarbeten med fastighetsägare ger annonsörer tillgång
+          till exponeringsytor där människor bor, arbetar och konsumerar.
+        </p>
       </div>
     </div>
   </div>
