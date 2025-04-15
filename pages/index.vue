0074840eecf2cc9e6dd3918ca17f59c57b6a701f
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-04-15 23:01:54 +0200

    restored page again and new test

--- pages/index.vue
+++ pages/index.vue
@@ -19,94 +19,88 @@ definePageMeta({
 </script>
 
 <template>
-  <ClientOnly>
-    <video autoplay loop muted class="h-full w-full object-cover">
-      <source src="../assets/videos/folkmassa.mp4" type="video/mp4" />
-    </video>
-  </ClientOnly>
+  <div
+    class="relative mx-[calc(-50vw+50%)] w-screen"
+    :style="{ height: `calc(100vh - ${navHeight}px)` }"
+  >
+    <LoadingSpinner
+      v-if="!videoLoaded"
+      class="absolute top-2/5 left-1/2"
+    ></LoadingSpinner>
 
-  <!--  <div-->
-  <!--    class="relative mx-[calc(-50vw+50%)] w-screen"-->
-  <!--    :style="{ height: `calc(100vh - ${navHeight}px)` }"-->
-  <!--  >-->
-  <!--    <LoadingSpinner-->
-  <!--      v-if="!videoLoaded"-->
-  <!--      class="absolute top-2/5 left-1/2"-->
-  <!--    ></LoadingSpinner>-->
+    <ClientOnly>
+      <video
+        v-show="videoLoaded"
+        @canplay="videoLoaded = true"
+        autoplay
+        loop
+        muted
+        disablepictureinpicture
+        playsinline
+        preload="auto"
+        class="h-full w-full object-cover"
+      >
+        <source src="../assets/videos/folkmassa.mp4" type="video/mp4" />
+      </video>
+    </ClientOnly>
 
-  <!--    <ClientOnly>-->
-  <!--      <video-->
-  <!--        v-show="videoLoaded"-->
-  <!--        @loadeddata="videoLoaded = true"-->
-  <!--        autoplay-->
-  <!--        loop-->
-  <!--        muted-->
-  <!--        disablepictureinpicture-->
-  <!--        playsinline-->
-  <!--        preload="auto"-->
-  <!--        class="h-full w-full object-cover"-->
-  <!--      >-->
-  <!--        <source src="../assets/videos/folkmassa.mp4" type="video/mp4" />-->
-  <!--      </video>-->
-  <!--    </ClientOnly>-->
+    <div v-show="videoLoaded" class="absolute inset-0 bg-black/40"></div>
 
-  <!--    <div v-show="videoLoaded" class="absolute inset-0 bg-black/40"></div>-->
+    <div
+      v-show="videoLoaded"
+      class="absolute inset-0 flex flex-col items-center justify-center bg-amber-700/15 text-center"
+    >
+      <h1 class="mb-8 text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+        Vi hjälper er att nå ut digitalt!
+      </h1>
+      <div class="px-8 text-lg lg:text-2xl">
+        Lokalt, regionalt eller rikstäckande - just DOOH* it!
+      </div>
 
-  <!--    <div-->
-  <!--      v-show="videoLoaded"-->
-  <!--      class="absolute inset-0 flex flex-col items-center justify-center bg-amber-700/15 text-center"-->
-  <!--    >-->
-  <!--      <h1 class="mb-8 text-3xl sm:text-4xl md:text-5xl lg:text-6xl">-->
-  <!--        Vi hjälper er att nå ut digitalt!-->
-  <!--      </h1>-->
-  <!--      <div class="px-8 text-lg lg:text-2xl">-->
-  <!--        Lokalt, regionalt eller rikstäckande - just DOOH* it!-->
-  <!--      </div>-->
+      <div class="text-l absolute right-3 bottom-2 italic opacity-30">
+        *DOOH - Digital out of Home Media
+      </div>
 
-  <!--      <div class="text-l absolute right-3 bottom-2 italic opacity-30">-->
-  <!--        *DOOH - Digital out of Home Media-->
-  <!--      </div>-->
+      <NuxtLink
+        :to="{ path: '/', hash: '#services' }"
+        class="absolute bottom-16 flex w-full items-center justify-center"
+      >
+        <ChevronDoubleDownIcon
+          class="h-12 min-h-12 w-12 min-w-12 cursor-pointer opacity-70 hover:opacity-100"
+        ></ChevronDoubleDownIcon>
+      </NuxtLink>
+    </div>
+  </div>
 
-  <!--      <NuxtLink-->
-  <!--        :to="{ path: '/', hash: '#services' }"-->
-  <!--        class="absolute bottom-16 flex w-full items-center justify-center"-->
-  <!--      >-->
-  <!--        <ChevronDoubleDownIcon-->
-  <!--          class="h-12 min-h-12 w-12 min-w-12 cursor-pointer opacity-70 hover:opacity-100"-->
-  <!--        ></ChevronDoubleDownIcon>-->
-  <!--      </NuxtLink>-->
-  <!--    </div>-->
-  <!--  </div>-->
+  <div id="services" class="mx-8 my-20 flex flex-col gap-8">
+    <VideoBlob></VideoBlob>
 
-  <!--  <div id="services" class="mx-8 my-20 flex flex-col gap-8">-->
-  <!--    <VideoBlob></VideoBlob>-->
+    <div class="mt-12 grid gap-8 md:grid-cols-2">
+      <IconBlob
+        icon="tv"
+        message="Butiks-TV är ett effektivt marknadsföringsverktyg som visar reklam och information direkt i butiksmiljön. Skärmarna placeras strategiskt för att nå kunder när de handlar."
+        link="/butiks-tv"
+      ></IconBlob>
 
-  <!--    <div class="mt-12 grid gap-8 md:grid-cols-2">-->
-  <!--      <IconBlob-->
-  <!--        icon="tv"-->
-  <!--        message="Butiks-TV är ett effektivt marknadsföringsverktyg som visar reklam och information direkt i butiksmiljön. Skärmarna placeras strategiskt för att nå kunder när de handlar."-->
-  <!--        link="/butiks-tv"-->
-  <!--      ></IconBlob>-->
+      <IconBlob
+        icon="computer"
+        message="Digitala utomhusskärmar eller LED-skyltar visar rörlig reklam som effektivt fångar förbipasserande trafikanters uppmärksamhet. Med sina starka färger och dynamiska innehåll skapar de minnesvärda intryck även vid hög hastighet."
+        link="/storbildsskarmar"
+      ></IconBlob>
 
-  <!--      <IconBlob-->
-  <!--        icon="computer"-->
-  <!--        message="Digitala utomhusskärmar eller LED-skyltar visar rörlig reklam som effektivt fångar förbipasserande trafikanters uppmärksamhet. Med sina starka färger och dynamiska innehåll skapar de minnesvärda intryck även vid hög hastighet."-->
-  <!--        link="/storbildsskarmar"-->
-  <!--      ></IconBlob>-->
+      <IconBlob
+        icon="video"
+        message="Med specialistkompetens kring olika kanaler maximeras avkastningen på kundens marknadsföringsinvesteringar genom datadrivna beslut och förhandlingskraft gentemot medieleverantörer."
+        link="/mediabyraer"
+      ></IconBlob>
 
-  <!--      <IconBlob-->
-  <!--        icon="video"-->
-  <!--        message="Med specialistkompetens kring olika kanaler maximeras avkastningen på kundens marknadsföringsinvesteringar genom datadrivna beslut och förhandlingskraft gentemot medieleverantörer."-->
-  <!--        link="/mediabyraer"-->
-  <!--      ></IconBlob>-->
-
-  <!--      <IconBlob-->
-  <!--        icon="building"-->
-  <!--        message="Strategiska samarbeten med fastighetsägare ger annonsörer tillgång till exponeringsytor där människor bor, arbetar och konsumerar."-->
-  <!--        link="/kommersialisera"-->
-  <!--      ></IconBlob>-->
-  <!--    </div>-->
-  <!--  </div>-->
+      <IconBlob
+        icon="building"
+        message="Strategiska samarbeten med fastighetsägare ger annonsörer tillgång till exponeringsytor där människor bor, arbetar och konsumerar."
+        link="/kommersialisera"
+      ></IconBlob>
+    </div>
+  </div>
 </template>
 
 <script>
