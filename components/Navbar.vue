19d86995427203d693c60d6afae71685e4c680b2
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-04-21 18:37:19 +0200

    more styling changes

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,89 +7,104 @@ import { Bars2Icon, XMarkIcon } from "@heroicons/vue/24/outline";
   <div
     id="navbar"
     ref="navbar"
-    class="relative z-10 flex items-center justify-between bg-black p-4 py-2"
+    class="relative z-10 mx-[calc(-50vw+50%)] w-screen bg-neutral-950"
   >
-    <router-link to="/">
-      <NuxtImg
-        src="adinq_vit.svg"
-        alt="company logo"
-        class="h-auto w-24 p-1 xl:w-32 xl:p-2"
-      />
-    </router-link>
-
-    <Bars2Icon
-      @click="showNavbar = !showNavbar"
-      class="block h-8 w-8 cursor-pointer xl:hidden"
-    />
-
     <div
-      class="fixed top-0 right-0 bottom-0 left-auto flex flex-col items-end justify-start gap-5 px-8 pt-24 pb-10 text-center xl:static xl:flex-row xl:items-center xl:bg-transparent xl:p-0"
-      :class="showNavbar ? 'absolute bg-black xl:flex' : 'hidden xl:flex'"
+      class="mx-auto flex w-full max-w-screen-2xl items-center justify-between p-4 py-2"
     >
-      <XMarkIcon
-        v-if="showNavbar"
+      <router-link to="/">
+        <NuxtImg
+          src="adinq_vit.svg"
+          alt="company logo"
+          class="h-auto w-24 p-1 xl:w-32 xl:p-2"
+        />
+      </router-link>
+
+      <Bars2Icon
         @click="showNavbar = !showNavbar"
-        class="absolute top-4 right-4 h-8 w-8 cursor-pointer"
+        class="block h-8 w-8 cursor-pointer xl:hidden"
       />
 
-      <NavbarLink
-        icon="HomeIcon"
-        text="Hem"
-        route="/"
-        :class="showNavbar && 'flex-row-reverse'"
-      ></NavbarLink>
+      <!--      <div-->
+      <!--        class="fixed top-0 right-0 bottom-0 left-auto flex flex-col items-end justify-start gap-5 px-8 pt-24 pb-10 text-center transition-all duration-1000 ease-in-out xl:static xl:flex-row xl:items-center xl:bg-transparent xl:p-0"-->
+      <!--        :class="-->
+      <!--          showNavbar-->
+      <!--            ? 'absolute translate-x-0 bg-neutral-950 xl:flex'-->
+      <!--            : 'absolute translate-x-100 xl:flex'-->
+      <!--        "-->
+      <!--      >-->
 
-      <NavbarLink
-        icon="FilmIcon"
-        text="Mediaproduktion"
-        route="/media-produktion"
-        :class="showNavbar && 'flex-row-reverse'"
-      ></NavbarLink>
-
-      <NavbarLink
-        icon="UserGroupIcon"
-        text="Medarbetare"
-        route="/medarbetare"
-        :class="showNavbar && 'flex-row-reverse'"
-      ></NavbarLink>
-
-      <NavbarLink
-        icon="ChatBubbleOvalLeftEllipsisIcon"
-        text="Kundutlåtanden"
-        route="/kundutlatanden"
-        :class="showNavbar && 'flex-row-reverse'"
-      ></NavbarLink>
-
-      <NavbarLink
-        icon="QuestionMarkCircleIcon"
-        text="Varför oss"
-        route="/varfor-oss"
-        :class="showNavbar && 'flex-row-reverse'"
-      ></NavbarLink>
-
-      <NavbarLink
-        icon="CurrencyEuroIcon"
-        text="Priser"
-        route="/priser"
-        :class="showNavbar && 'flex-row-reverse'"
-      ></NavbarLink>
-
-      <div class="group relative">
-        <Button
-          @click="showNavbar = false"
-          text="Kontakta oss"
-          link="/kontakta-oss"
-          type="button"
-          styling="light"
-          class="w-full opacity-90 transition-opacity duration-300 ease-in-out hover:opacity-100"
+      <div
+        class="fixed top-0 right-0 bottom-0 left-auto flex flex-col items-end justify-start gap-5 px-8 pt-24 pb-10 text-center xl:static xl:flex-row xl:items-center xl:bg-transparent xl:p-0"
+        :class="
+          showNavbar ? 'absolute bg-neutral-950 xl:flex' : 'hidden xl:flex'
+        "
+      >
+        <XMarkIcon
+          v-if="showNavbar"
+          @click="showNavbar = !showNavbar"
+          class="absolute top-4 right-4 h-8 w-8 cursor-pointer"
         />
-        <div
-          class="absolute inset-0 z-[-1] w-full rounded-full bg-gradient-to-r from-blue-500 to-green-500 opacity-0 blur-xl transition-all duration-300 ease-in-out group-hover:opacity-100"
-          :class="[
-            currentPath === '/kontakta-oss' &&
-              'bg-gradient-to-r from-pink-500 to-purple-500 opacity-50',
-          ]"
-        />
+
+        <NavbarLink
+          icon="HomeIcon"
+          text="Hem"
+          route="/"
+          :class="showNavbar && 'flex-row-reverse'"
+        ></NavbarLink>
+
+        <NavbarLink
+          icon="FilmIcon"
+          text="Mediaproduktion"
+          route="/media-produktion"
+          :class="showNavbar && 'flex-row-reverse'"
+        ></NavbarLink>
+
+        <NavbarLink
+          icon="UserGroupIcon"
+          text="Medarbetare"
+          route="/medarbetare"
+          :class="showNavbar && 'flex-row-reverse'"
+        ></NavbarLink>
+
+        <NavbarLink
+          icon="ChatBubbleOvalLeftEllipsisIcon"
+          text="Kundutlåtanden"
+          route="/kundutlatanden"
+          :class="showNavbar && 'flex-row-reverse'"
+        ></NavbarLink>
+
+        <NavbarLink
+          icon="QuestionMarkCircleIcon"
+          text="Varför oss"
+          route="/varfor-oss"
+          :class="showNavbar && 'flex-row-reverse'"
+        ></NavbarLink>
+
+        <NavbarLink
+          icon="CurrencyEuroIcon"
+          text="Priser"
+          route="/priser"
+          :class="showNavbar && 'flex-row-reverse'"
+        ></NavbarLink>
+
+        <div class="group relative">
+          <Button
+            @click="showNavbar = false"
+            text="Kontakta oss"
+            link="/kontakta-oss"
+            type="button"
+            styling="light"
+            class="w-full opacity-90 transition-opacity duration-300 ease-in-out hover:opacity-100"
+          />
+          <div
+            class="absolute inset-0 z-[-1] w-full rounded-full bg-gradient-to-r from-blue-500 to-green-500 opacity-0 blur-xl transition-all duration-300 ease-in-out group-hover:opacity-100"
+            :class="[
+              currentPath === '/kontakta-oss' &&
+                'bg-gradient-to-r from-pink-500 to-purple-500 opacity-50',
+            ]"
+          />
+        </div>
       </div>
     </div>
   </div>
