bbf89e2e2be71f52685081c2cfa2124bd7a66d53
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-04-27 23:36:18 +0200

    started on media production page

--- pages/media-produktion.vue
+++ pages/media-produktion.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ChevronDoubleDownIcon } from "@heroicons/vue/24/solid";
+
 useSeoMeta({
   title: "",
   description: "",
@@ -17,11 +19,133 @@ definePageMeta({
 </script>
 
 <template>
-  <!--  <Home />-->
+  <div
+    class="relative mx-[calc(-50vw+50%)] w-screen"
+    :style="{
+      height: `calc(100${supportsDvh ? 'dvh' : 'vh'} - ${navHeight}px)`,
+    }"
+  >
+    <NuxtImg
+      src="andrey-novik-5DDDzKMK0s8-unsplash.jpg"
+      alt=""
+      class="h-full w-full object-cover"
+      sizes="1000px md:2000px"
+      width="4600"
+      height="3500"
+      densities="x1"
+    />
+
+    <div class="absolute inset-0 bg-black/40"></div>
+
+    <div
+      class="absolute inset-0 flex flex-col items-center justify-center bg-amber-700/15 text-center"
+    >
+      <h1 class="mb-8 text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+        Vi producerar er reklamfilm eller slide för storbildsskärmar!
+      </h1>
+      <div class="px-8 text-lg lg:text-2xl">
+        -vi kan hjälpa er med allt under samma tak!
+      </div>
+
+      <NuxtLink
+        :to="{ path: '/', hash: '#services' }"
+        class="absolute bottom-24 flex w-full items-center justify-center sm:bottom-16"
+      >
+        <ChevronDoubleDownIcon
+          class="h-12 min-h-12 w-12 min-w-12 cursor-pointer opacity-70 hover:opacity-100"
+        ></ChevronDoubleDownIcon>
+      </NuxtLink>
+    </div>
+  </div>
+
+  <div class="mx-4 my-20 flex flex-col gap-8 sm:mx-8">
+    <h3 id="services" class="mt-32 text-center">Se vår informationsvideo</h3>
+    <div class="flex justify-center gap-4 underline">
+      <div
+        @click="sweVersion = true"
+        class="cursor-pointer"
+        :class="[!sweVersion && 'opacity-25']"
+      >
+        Svensk version
+      </div>
+      <div class="opacity-50">|</div>
+      <div
+        @click="sweVersion = false"
+        class="cursor-pointer"
+        :class="[sweVersion && 'opacity-25']"
+      >
+        English version
+      </div>
+    </div>
+
+    <VideoBlob
+      v-if="sweVersion"
+      video-Link="/videos/ADinQ-RiksSWE.mp4"
+    ></VideoBlob>
+    <VideoBlob
+      v-if="!sweVersion"
+      video-Link="/videos/ADinQ-RiksENG.mp4"
+    ></VideoBlob>
+
+    <h3 class="mt-32 text-center">
+      Mer information om våra marknadsföringskanaler
+    </h3>
+
+    <div class="mt-4 grid gap-8 md:grid-cols-2">
+      <IconBlob
+        icon="tv"
+        message="Butiks-TV är ett effektivt marknadsföringsverktyg som visar reklam och information direkt i butiksmiljön. Skärmarna placeras strategiskt för att nå kunder när de handlar."
+        link="/butiks-tv"
+      ></IconBlob>
+
+      <IconBlob
+        icon="computer"
+        message="Digitala utomhusskärmar eller LED-skyltar visar rörlig reklam som effektivt fångar förbipasserande trafikanters uppmärksamhet. Med sina starka färger och dynamiska innehåll skapar de minnesvärda intryck även vid hög hastighet."
+        link="/storbildsskarmar"
+      ></IconBlob>
+
+      <IconBlob
+        icon="video"
+        message="Med specialistkompetens kring olika kanaler maximeras avkastningen på kundens marknadsföringsinvesteringar genom datadrivna beslut och förhandlingskraft gentemot medieleverantörer."
+        link="/mediabyraer"
+      ></IconBlob>
+
+      <IconBlob
+        icon="building"
+        message="Strategiska samarbeten med fastighetsägare ger annonsörer tillgång till exponeringsytor där människor bor, arbetar och konsumerar."
+        link="/kommersialisera"
+      ></IconBlob>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "MediaProduktion",
+
+  inject: ["navbarHeight"],
+
+  data() {
+    return {
+      sweVersion: true,
+      supportsDvh: null,
+    };
+  },
+
+  activated() {
+    if (this.$refs.titleVideo) {
+      this.$refs.titleVideo.play();
+    }
+  },
+
+  computed: {
+    navHeight() {
+      return this.navbarHeight();
+    },
+  },
+
+  mounted() {
+    this.supportsDvh = CSS.supports("height", "100dvh");
+  },
 };
 </script>
