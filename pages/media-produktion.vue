a459101ac7b7a63721fd163ce9a5560e64533556
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-05-10 18:12:33 +0200

    content work

--- pages/media-produktion.vue
+++ pages/media-produktion.vue
@@ -50,7 +50,7 @@ definePageMeta({
       </div>
 
       <NuxtLink
-        :to="{ path: '/', hash: '#services' }"
+        :to="{ path: '/media-produktion', hash: '#info' }"
         class="absolute bottom-24 flex w-full items-center justify-center sm:bottom-16"
       >
         <ChevronDoubleDownIcon
@@ -59,6 +59,57 @@ definePageMeta({
       </NuxtLink>
     </div>
   </div>
+
+  <div id="info" class="mx-4 my-20 flex flex-col gap-8 sm:mx-8">
+    <p>
+      Stort eller litet företag? Vi har lösningar för det mesta. Nej vi kan inte
+      trolla men vi har en bred kompetens och vi försöker alltid skapa bästa
+      möjliga produktion utifrån ditt företags behov och plånbok här och nu. Vi
+      hjälper er gärna!
+    </p>
+    <p>
+      Vi gör både reklamfilmer och material till storbildsskärmar i olika
+      storlek och komplexitet för våra digitala utomhusmedier.
+    </p>
+    <p>
+      Tänk på att nyttan med en bra film är större än bara en enskild kampanj.
+      Ni kan även fritt använda er film i sociala medier och nå ännu fler
+      människor samtidigt som ni stärker ert varumärke.
+    </p>
+
+    <VideoBlob video-Link="/videos/ADinQ-Info.mp4"></VideoBlob>
+
+    <h3 class="mt-32 text-center">
+      Mer information om våra marknadsföringskanaler
+    </h3>
+
+    <div class="mt-4 grid gap-8 md:grid-cols-2">
+      <IconBlob
+        icon="tv"
+        message="Butiks-TV är ett effektivt marknadsföringsverktyg som visar reklam och information direkt i butiksmiljön. Skärmarna placeras strategiskt för att nå kunder när de handlar."
+        link="/butiks-tv"
+      ></IconBlob>
+
+      <IconBlob
+        icon="computer"
+        message="Digitala utomhusskärmar eller LED-skyltar visar rörlig reklam som effektivt fångar förbipasserande trafikanters uppmärksamhet. Med sina starka färger och dynamiska innehåll skapar de minnesvärda intryck även vid hög hastighet."
+        link="/storbildsskarmar"
+      ></IconBlob>
+
+      <IconBlob
+        icon="video"
+        message="Med specialistkompetens kring olika kanaler maximeras avkastningen på kundens marknadsföringsinvesteringar genom datadrivna beslut och förhandlingskraft gentemot medieleverantörer."
+        link="/mediabyraer"
+      ></IconBlob>
+
+      <IconBlob
+        icon="building"
+        message="Strategiska samarbeten med fastighetsägare ger annonsörer tillgång till exponeringsytor där människor bor, arbetar och konsumerar."
+        link="/kommersialisera"
+      ></IconBlob>
+    </div>
+  </div>
+  s
 </template>
 
 <script>
