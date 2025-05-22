3057b887e84986a6601f6887a21b4699a5a1403c
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-05-22 09:14:18 +0200

    started working on medarbetare page

--- pages/medarbetare.vue
+++ pages/medarbetare.vue
@@ -17,7 +17,21 @@ definePageMeta({
 </script>
 
 <template>
-  <!--  <Home />-->
+  <div class="relative mx-[calc(-50vw+50%)] w-screen">
+    <NuxtImg
+      src="teo-d-4op9_2Bt2Eg-unsplash.jpg"
+      alt=""
+      class="h-full w-full object-cover"
+      sizes="1000px md:2000px"
+      width="3456"
+      height="5184"
+      densities="x1"
+    />
+
+    <div class="absolute inset-0 bg-[#365e80]/70"></div>
+  </div>
+
+  <div class="mx-4 my-20 flex flex-col gap-8 sm:mx-8">HEJ</div>
 </template>
 
 <script>
