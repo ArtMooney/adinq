0d4ee1b88dd44e4de27b830100a1c348d2f469c6
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-06-25 08:56:39 +0200

    removed non tailwind breakpoints

--- mixins/getBreakpoint.js
+++ mixins/getBreakpoint.js
@@ -1,46 +1,33 @@
 export const getBreakpointMixin = {
-    data() {
-        return {
-            breakpoints: {
-                xs: true,
-                phone: false,
-                sm: false,
-                md: false,
-                laptop: false,
-                lg: false,
-                xlarge: false,
-                xl: false,
-                xxlarge: false,
-                desktop: false,
-                "2xl": false,
-                wide: false,
-            },
-        };
-    },
+  data() {
+    return {
+      breakpoints: {
+        sm: false,
+        md: false,
+        lg: false,
+        xl: false,
+        "2xl": false,
+      },
+    };
+  },
 
-    methods: {
-        updateBreakpoints() {
-            const width = window.innerWidth;
-            this.breakpoints.phone = width >= 576;
-            this.breakpoints.sm = width >= 640;
-            this.breakpoints.md = width >= 768;
-            this.breakpoints.laptop = width >= 992;
-            this.breakpoints.lg = width >= 1024;
-            this.breakpoints.xlarge = width >= 1200;
-            this.breakpoints.xl = width >= 1280;
-            this.breakpoints.xxlarge = width >= 1440;
-            this.breakpoints.desktop = width >= 1500;
-            this.breakpoints["2xl"] = width >= 1536;
-            this.breakpoints.wide = width >= 2048;
-        },
+  methods: {
+    updateBreakpoints() {
+      const width = window.innerWidth;
+      this.breakpoints.sm = width >= 640;
+      this.breakpoints.md = width >= 768;
+      this.breakpoints.lg = width >= 1024;
+      this.breakpoints.xl = width >= 1280;
+      this.breakpoints["2xl"] = width >= 1536;
     },
+  },
 
-    mounted() {
-        this.updateBreakpoints();
-        window.addEventListener("resize", this.updateBreakpoints);
-    },
+  mounted() {
+    this.updateBreakpoints();
+    window.addEventListener("resize", this.updateBreakpoints);
+  },
 
-    beforeUnmount() {
-        window.removeEventListener("resize", this.updateBreakpoints);
-    },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.updateBreakpoints);
+  },
 };
