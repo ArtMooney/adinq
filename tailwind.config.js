44f67411aaa64cd0dbe9b4fb2a04355e64cd57d8
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-07-09 10:13:25 +0200

    styled buttons directly in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,6 +50,58 @@ export default {
           "@apply text-base md:text-lg lg:text-xl leading-5 md:leading-9 lg:leading-11":
             {},
         },
+
+        "button, .button": {
+          "@apply rounded px-8 py-3 text-center text-sm no-underline cursor-pointer transition-colors duration-300 ease-in-out":
+            {},
+          "&:disabled, &.disabled": {
+            "@apply !cursor-not-allowed !pointer-events-none": {},
+          },
+        },
+
+        "button.primary, .button.primary": {
+          "@apply bg-neutral-800 text-white": {},
+          "&:hover": {
+            "@apply bg-neutral-800/80": {},
+          },
+          "&:focus, &.selected, &:active": {
+            "@apply bg-neutral-800 outline-4 -outline-offset-4": {},
+          },
+          "&:disabled, &.disabled": {
+            "@apply bg-neutral-700 text-neutral-500 ": {},
+          },
+        },
+
+        "button.secondary, .button.secondary": {
+          "@apply bg-transparent text-white outline-white outline-2 -outline-offset-2":
+            {},
+          "&:hover, &:focus, &.selected, &:active": {
+            "@apply outline-white/80": {},
+          },
+          "&:disabled, &.disabled": {
+            "@apply bg-neutral-800 text-neutral-600 outline-neutral-600": {},
+          },
+        },
+
+        "button.white, .button.white": {
+          "@apply bg-white/95 text-black outline-black": {},
+          "&:hover, &:focus, &.selected, &:active": {
+            "@apply bg-white/80": {},
+          },
+          "&:disabled, &.disabled": {
+            "@apply bg-neutral-300 text-neutral-400": {},
+          },
+        },
+
+        "button.transparent, .button.transparent": {
+          "@apply bg-transparent text-white": {},
+          "&:hover, &:focus, &.selected, &:active": {
+            "@apply bg-white/10": {},
+          },
+          "&:disabled, &.disabled": {
+            "@apply bg-neutral-800 text-neutral-600": {},
+          },
+        },
       });
     }),
   ],
