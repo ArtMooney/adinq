a41849f8240df7229f1be3ecc1bfbb01fcea8651
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-08-03 22:58:14 +0200

    started connecting cms from admin page

---
+++ pages/admin.vue
@@ -0,0 +1,56 @@
+<script setup>
+useSeoMeta({
+  title: "",
+  description: "",
+  ogTitle: "",
+  ogDescription: "",
+  ogImage: "",
+  twitterCard: "",
+  twitterTitle: "",
+  twitterDescription: "",
+  twitterImage: "",
+});
+
+definePageMeta({
+  ssr: false,
+});
+</script>
+
+<template>
+  <div class="flex h-full w-full grow items-center justify-center">
+    <CmsLogin
+      v-if="panel === 'login'"
+      @status="handleLoginStatus"
+      class="mx-auto w-full max-w-screen-xl"
+    />
+    <CmsMain v-if="panel === 'cms'" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Admin",
+
+  data() {
+    return {
+      panel: "login",
+    };
+  },
+
+  created() {
+    if (getLocalStorage("adinq-cms")) {
+      this.panel = "cms";
+    }
+  },
+
+  methods: {
+    handleLoginStatus(status) {
+      if (status === "ok") {
+        this.panel = "cms";
+      } else {
+        this.panel = "login";
+      }
+    },
+  },
+};
+</script>
