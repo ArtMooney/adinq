5609c59fcf073ea4c7c50c377ffa8f6ed74a4bd3
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-08-14 14:32:12 +0200

    updated the loading spinner component

--- components/cms/LoadingSpinner.vue
+++ components/cms/LoadingSpinner.vue
@@ -1,19 +1,30 @@
 <template>
   <div
-    class="spinner h-10 w-10 [animation:spin_0.6s_linear_infinite] rounded-full border-[3px] border-white/20 border-t-white"
+    class="[animation:spin_0.6s_linear_infinite] rounded-full border-[3px]"
+    :class="[
+      size === 'large' ? 'h-10 w-10' : 'h-5 w-5',
+      mode === 'dark'
+        ? 'border-white/20 border-t-white'
+        : 'border-black/20 border-t-black',
+    ]"
   ></div>
 </template>
 
 <script>
 export default {
   name: "LoadingSpinner",
+
+  props: {
+    size: {
+      type: String,
+      required: false,
+      default: "default",
+    },
+    mode: {
+      type: String,
+      required: false,
+      default: "dark",
+    },
+  },
 };
 </script>
-
-<style scoped>
-@keyframes spin {
-  to {
-    transform: rotate(360deg);
-  }
-}
-</style>
