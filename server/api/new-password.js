4c806b61fd753508db2aaecd8068872150da97a0
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-08-15 10:16:55 +0200

    fixed new password api for nuxt

---
+++ server/api/new-password.js
@@ -0,0 +1,83 @@
+import { checkLogin } from "../utils/check-login.js";
+import { listTables } from "../db/baserow/list-tables.js";
+import { listRows } from "../db/baserow/list-rows.js";
+import { updateRow } from "../db/baserow/update-row.js";
+import { sendEmail } from "~/server/utils/mailgun/send-email.js";
+import { messageNewPassword } from "../content/message-new-password.js";
+
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig();
+  const headers = getHeaders(event);
+
+  if (!(await checkLogin(headers, config.userName, config.userPass))) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized",
+    });
+  }
+
+  const body = await readBody(event);
+
+  if (!body.password || !body.validation) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Missing password or validation",
+    });
+  }
+
+  const schema = await listTables(
+    config.baserowUsername,
+    config.baserowPassword,
+    config.baserowDbId,
+  );
+
+  const usersId = schema.find((table) => table.name === "CMS users")?.id;
+
+  if (!usersId) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: "CMS users table not found",
+    });
+  }
+
+  const user = await listRows(
+    config.baserowToken,
+    usersId,
+    null,
+    null,
+    body.validation,
+  );
+
+  if (!user || user.results.length === 0) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Error validating account",
+    });
+  }
+
+  user.results[0].password = body.password;
+  user.results[0]["reset-id"] = "";
+  const savePassword = await updateRow(
+    config.baserowToken,
+    usersId,
+    user.results[0].id,
+    user.results[0],
+  );
+
+  if (savePassword.error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Error saving password",
+    });
+  }
+
+  const sendToEmail = await sendEmail(
+    config.emailFrom,
+    savePassword.email,
+    "Your password to log into Simple CMS was changed",
+    await messageNewPassword(),
+    config.mailgunApiKey,
+  );
+
+  return "ok";
+});
