657588fa6f9502e4914f5a0148a4ed397aa79f7d
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-08-17 21:12:36 +0200

    optimized upload file code for nuxt

--- server/db/baserow/upload-file.js
+++ server/db/baserow/upload-file.js
@@ -1,44 +1,27 @@
 export async function uploadFile(token, fileName, base64Data) {
+  const url = "https://api.baserow.io/api/user-files/upload-file/";
   const byteCharacters = atob(base64Data);
-  let byteNumbers = new Array(byteCharacters.length);
-
-  for (let i = 0; i < byteCharacters.length; i++) {
-    byteNumbers[i] = byteCharacters.charCodeAt(i);
-  }
-
+  const byteNumbers = Array.from(byteCharacters, (char) => char.charCodeAt(0));
   const byteArray = new Uint8Array(byteNumbers);
   const blob = new Blob([byteArray], { type: "application/octet-stream" });
 
   let bodyContent = new FormData();
   bodyContent.append("file", blob, fileName);
 
-  let headersList = {
-    Accept: "*/*",
-    Authorization: "Token " + token,
-  };
+  try {
+    return await $fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "*/*",
+        Authorization: `Token ${token}`,
+      },
+      body: bodyContent,
 
-  for (let attempt = 1; attempt <= 3; attempt++) {
-    try {
-      let response = await fetch(
-        "https://api.baserow.io/api/user-files/upload-file/",
-        {
-          method: "POST",
-          body: bodyContent,
-          headers: headersList,
-        },
-      );
-
-      if (!response.ok) {
-        return { error: `HTTP error! status: ${response.status}` };
-      }
-
-      return await response.json();
-    } catch (error) {
-      if (attempt < 3) {
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-      } else {
-        return { error: `Network error: ${error.message}` };
-      }
-    }
+      retry: 3,
+      retryDelay: 1000,
+      retryStatusCodes: [408, 429, 500, 502, 503, 504],
+    });
+  } catch (error) {
+    throw new Error(`Network error: ${error.message}`);
   }
 }
