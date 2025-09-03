7057e21995773b9bd4f7dde34ada0adb33954f3a
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-09-03 08:13:25 +0200

    removed old website from main branch

--- website-backup/@includes/api-qcard.js
+++
@@ -1,6 +0,0 @@
-var filmurl = "@videos/74464_coco_thai_1901.mp4"; // Byt URL'en mot pekare och lokal filmfil.
-var producent = "mattias.sundberg@adinq.se"; // Byt namnet mot rätt producent's mailaddress.
-var filmOk = 0; // 0 = Inget valt, 1 = Ok, 2 = Inte Ok
-  
-var filename = filmurl.split('/').pop().split('#')[0].split('?')[0];
-var filmProducent = "mailto: " + producent + "?subject=Kommentarer%20ang%20" + filename;
