de30a89d9d04adfd5786dd5ca8393388d726e44a
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-10-20 17:17:26 +0200

    updated client setup for easier use

--- server/db/client.ts
+++ server/db/client.ts
@@ -1,13 +1,7 @@
 import { drizzle } from "drizzle-orm/d1";
-import { users, mediaproduktioner } from "./schema";
+import * as schema from "./schema";
 import type { D1Database } from "@cloudflare/workers-types";
 
-declare module "#drizzle" {
-  interface DrizzleEnv {
-    DB: D1Database;
-  }
-}
-
-export const db = drizzle(import.meta.env.DB as D1Database, {
-  schema: { users, mediaproduktioner },
-});
+export const useDrizzle = (db: D1Database) => {
+  return drizzle(db, { schema });
+};
