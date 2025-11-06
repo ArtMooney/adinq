ffb48b1827decc60510f29e8f2fa508766c314d5
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-11-06 11:11:46 +0100

    connected field-types from schema to fields api

--- server/api/cms/fields.js
+++ server/api/cms/fields.js
@@ -1,7 +1,7 @@
 import { checkLogin } from "~~/server/utils/check-login.js";
 import { checkAuthentication } from "~~/server/utils/check-authentication.js";
 import * as schema from "~~/server/db/schema.ts";
-import { cmsTables } from "~~/server/db/schema.ts";
+import { cmsTables, fieldTypes } from "~~/server/db/schema.ts";
 import { getTableColumns } from "drizzle-orm";
 
 export default defineEventHandler(async (event) => {
@@ -24,14 +24,17 @@ export default defineEventHandler(async (event) => {
   }
 
   const tableName = body?.table_id;
+
   if (!cmsTables.some((t) => t.id === tableName)) {
     throw createError({ statusCode: 400, statusMessage: "Invalid table" });
   }
+
   const table = schema[tableName];
   const columns = getTableColumns(table);
+  const fields = fieldTypes[tableName];
 
   return Object.keys(columns).map((key) => ({
     name: key,
-    type: columns[key].getSQLType(),
+    type: fields[key],
   }));
 });
