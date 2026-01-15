cdec14b79bfe3e88b070e96972dbd70000369761
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2026-01-15 23:31:09 +0100

    styling type rewrite

--- app/components/LoadingSpinner.vue
+++ app/components/LoadingSpinner.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="spinner h-10 w-10 [animation:spin_0.6s_linear_infinite] rounded-full border-[3px] border-white/20 border-t-white"
+    class="spinner h-10 w-10 animate-[spin_0.6s_linear_infinite] rounded-full border-[3px] border-white/20 border-t-white"
   ></div>
 </template>
 
